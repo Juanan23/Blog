6ac8dc3c17afe4d132ecee3859d498649355a69c
juan <juanan5febrero@gmail.com>
2021-05-24 15:29:21 +0200

    Creación componentes

---
+++ Blog/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { FormularioComponent } from './formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
